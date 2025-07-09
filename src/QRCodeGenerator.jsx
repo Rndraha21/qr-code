910bc58731b77d2b3d4af1458b076761266b47bd
Rndraha21 <robinndraha111@gmail.com>
2025-07-09 20:22:18 +0700

    update

--- src/QRCodeGenerator.jsx
+++ src/QRCodeGenerator.jsx
@@ -41,13 +41,13 @@ function QRCodeGenerator() {
   };
 
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center p-4">
-      <div className="w-full relative max-w-md p-8 bg-card border rounded-3xl shadow-lg shadow-black/40 hover:shadow-2xl flex flex-col items-center">
-        <div className="flex flex-col items-center mb-6">
-          <div className="w-18 h-18 flex items-center justify-center border rounded-full bg-secondary text-secondary-foreground mb-2">
-            <BsQrCode className="text-white w-10 h-10" />
+    <div className="min-h-screen relative w-full flex flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md p-6 bg-card border rounded-3xl shadow-lg shadow-black/40 hover:shadow-2xl flex flex-col items-center">
+        <div className="flex flex-col items-center mb-4">
+          <div className="w-16 h-16 flex items-center justify-center border rounded-full bg-secondary text-secondary-foreground mb-2">
+            <BsQrCode className="text-white w-8 h-8" />
           </div>
-          <h1 className="text-xl font-bold text-primary mb-1">
+          <h1 className="text-lg font-bold text-primary mb-1">
             <Typewriter
               options={{
                 strings: ["QR Code Generator"],
@@ -63,35 +63,35 @@ function QRCodeGenerator() {
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
           placeholder="Type text or URL"
-          className="w-full mb-5 px-4 py-3 bg-secondary text-primary-foreground border rounded-lg outline-none resize-none placeholder-gray-400 shadow-sm transition duration-400"
+          className="w-full mb-4 px-3 py-2 bg-secondary text-primary-foreground border rounded-lg outline-none resize-none placeholder-gray-400 shadow-sm transition duration-400"
         />
-        <div className="mb-5 flex items-center justify-center w-full">
-          <div className="bg-secondary border rounded-xl shadow-sm flex items-center justify-center p-4 min-h-[200px] w-full">
+        <div className="mb-4 flex items-center justify-center w-full">
+          <div className="bg-secondary border rounded-xl shadow-sm flex items-center justify-center p-2 min-h-[150px] max-h-[200px] w-full">
             {inputText.trim() ? (
               <QRCodeCanvas
                 value={inputText}
-                size={150}
+                size={120}
                 fgColor="rgba(225,225,225,1)"
                 bgColor="rgba(0,0,0,0)"
                 className="rounded"
               />
             ) : (
-              <BsQrCode className="w-16 h-16 mb-2 text-gray-300" />
+              <BsQrCode className="w-12 h-12 mb-2 text-gray-300" />
             )}
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row w-full gap-3 mb-2">
+        <div className="flex flex-col sm:flex-row w-full gap-2 mb-2">
           <button
             onClick={handleDownload}
             disabled={!inputText}
-            className="flex-1 flex items-center justify-center gap-2 px-4 py-2 bg-card border text-white rounded-lg hover:bg-blue-600 disabled:bg-background disabled:text-gray-400 disabled:cursor-not-allowed transition cursor-pointer"
+            className="flex-1 flex items-center justify-center gap-2 px-3 py-2 bg-card border text-white rounded-lg hover:bg-blue-600 disabled:bg-background disabled:text-gray-400 disabled:cursor-not-allowed transition cursor-pointer"
           >
             <FiDownload className="w-4 h-4" /> Download
           </button>
           <button
             onClick={handleCopy}
             disabled={!inputText}
-            className={`flex-1 flex items-center justify-center gap-2 px-4 py-2 rounded-lg transition cursor-pointer disabled:cursor-not-allowed disabled:bg-background disabled:text-gray-400 ${
+            className={`flex-1 flex items-center justify-center gap-2 px-3 py-2 rounded-lg transition cursor-pointer disabled:cursor-not-allowed disabled:bg-background disabled:text-gray-400 ${
               copied
                 ? "bg-green-500 text-white focus:outline-none focus:border-none"
                 : "bg-card border text-white hover:text-gray-700 hover:bg-gray-200 focus:outline-none focus:border-none"
@@ -107,7 +107,7 @@ function QRCodeGenerator() {
           <button
             onClick={handleClear}
             disabled={!inputText}
-            className="flex-1 flex items-center justify-center gap-2 px-4 py-2 bg-card border text-white hover:text-red-500 rounded-lg hover:bg-red-200 disabled:bg-background disabled:text-gray-400 disabled:cursor-not-allowed transition cursor-pointer"
+            className="flex-1 flex items-center justify-center gap-2 px-3 py-2 bg-card border text-white hover:text-red-500 rounded-lg hover:bg-red-200 disabled:bg-background disabled:text-gray-400 disabled:cursor-not-allowed transition cursor-pointer"
           >
             <FiX className="w-4 h-4" /> Clear
           </button>
